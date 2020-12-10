b608cf2bcfd95b47e0c3c021394ced51f07248a0
Bedirhan Celayir <44666921+rokasta12@users.noreply.github.com>
2020-12-10 16:38:06 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# url-shortener
+Only for shortening urls.
+
+See demo deployed to Heroku [Demo](https://urlkisaltici.herokuapp.com/).
+
+
+MEVN Stack Used.
+
+dont forget to add .env file for your project.
+
+- clone repo
+  ```sh
+  git clone https://github.com/rokasta12/url-shortener.git
+  ```
+
+- install dependancies
+  ```sh
+  npm install
+  ```
+
+- run locally.
+  ```sh
+  npm run dev
+  ```
---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+let yup = require("yup");
+const { nanoid } = require("nanoid");
+const monk = require("monk");
+const path = require("path");
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
+
+require("dotenv").config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get("urls");
+urls.createIndex({ slug: 1 }, { unique: true });
+
+const app = express();
+app.enable("trust proxy");
+
+app.use(helmet());
+
+app.use(morgan("common"));
+app.use(cors());
+
+app.use(express.json());
+app.use(express.static("./public"));
+
+const notFoundPath = path.join(__dirname, "public/404.html");
+
+//redirect to url
+app.get("/:id", async (req, res) => {
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    return res.status(404).sendFile(notFoundPath);
+  } catch (error) {
+    return res.status(404).sendFile(notFoundPath);
+  }
+});
+
+const linkSchema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/^[\w\-]+$/i),
+  url: yup.string().trim().url(),
+});
+
+const slowerDownLimiter = slowDown({
+  windowMs: 30 * 1000,
+  delayAfter: 3,
+  delayMs: 500,
+});
+
+const rateLimiter = rateLimit({
+  windowMs: 30 * 1000,
+  max: 3,
+});
+
+// create a shory url
+app.post("/url", slowerDownLimiter, rateLimiter, async (req, res, next) => {
+  let url = req.body.url;
+  let slug = req.body.slug;
+
+  try {
+    await linkSchema.validate(slug, url);
+    const existingUrl = await urls.findOne({ url });
+    if (existingUrl) {
+      res.json(existingUrl);
+    } else {
+      if (!slug) {
+        slug = nanoid(5);
+      } else {
+        const existing = await urls.findOne({ slug });
+        if (existing) {
+          throw new Error("slug in use ");
+        }
+      }
+      slug = slug.toLowerCase();
+      const newUrl = {
+        url,
+        slug,
+      };
+      const created = await urls.insert(newUrl);
+      res.json(created);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(notFoundPath);
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : error.stack,
+  });
+});
+
+const port = process.env.PORT || 1337;
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
